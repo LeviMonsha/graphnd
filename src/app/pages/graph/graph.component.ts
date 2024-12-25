961cc19dfe5e5046fedf6e827ccd63e7f9fc9013
vladimsov <lonker.hass@yandex.ru>
2024-12-25 15:00:57 +0300

    Add ability to upload data for graphs

--- src/app/pages/graph/graph.component.ts
+++ src/app/pages/graph/graph.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 import { LineChartComponent } from './line-chart/line-chart.component';
+import { CsvUploadComponent } from '../../services/csv-upload/csv-upload.component';
 
 @Component({
   selector: 'app-graph',
   standalone: true,
-  imports: [LineChartComponent],
+  imports: [LineChartComponent, CsvUploadComponent],
   templateUrl: './graph.component.html',
 })
-export class GraphComponent {}
+export class GraphComponent {
+  chartData: any[] = [];
+
+  onDataLoaded(data: any[]) {
+    this.chartData = data;
+  }
+}
--- src/app/pages/graph/line-chart/line-chart.component.ts
+++ src/app/pages/graph/line-chart/line-chart.component.ts
@@ -1,31 +1,48 @@
-import { Component } from '@angular/core';
-import { PlotlyModule } from 'angular-plotly.js';
+import { Component, Input } from '@angular/core';
 import * as PlotlyJS from 'plotly.js-dist-min';
+import { PlotlyModule } from 'angular-plotly.js';
+import { CommonModule } from '@angular/common';
 
 PlotlyModule.plotlyjs = PlotlyJS;
 
 @Component({
   selector: 'app-line-chart',
-  standalone: true,
-  imports: [PlotlyModule],
-  template:
-    '<plotly-plot [data]="graph.data" [layout]="graph.layout"  [config]="config"></plotly-plot>',
+  imports: [CommonModule, PlotlyModule],
+  template: `
+    <plotly-plot
+      *ngIf="graph"
+      [data]="graph.data"
+      [layout]="graph.layout"
+      [config]="config"
+    ></plotly-plot>
+  `,
 })
 export class LineChartComponent {
-  public graph = {
-    data: [
-      {
-        x: [1, 2, 3],
-        y: [2, 6, 3],
-        type: 'scatter',
-        mode: 'lines+points',
-        marker: { color: 'red' },
-      },
-      { x: [1, 2, 3], y: [2, 5, 3], type: 'bar' },
-    ],
-    layout: { width: 320, height: 240, title: 'A Fancy Plot' },
-  };
+  @Input() set data(data: any[]) {
+    if (data && data.length > 0) {
+      this.prepareGraphData(data);
+    }
+  }
 
+  public graph: any;
+
+  private prepareGraphData(data: any[]) {
+    const xValues = data.map((row) => row.x);
+    const yValues = data.map((row) => row.y);
+
+    this.graph = {
+      data: [
+        {
+          x: xValues,
+          y: yValues,
+          type: 'scatter',
+          mode: 'lines+points',
+          marker: { color: 'red' },
+        },
+      ],
+      layout: { title: 'График из CSV', width: 320, height: 240 },
+    };
+  }
   public config = {
     staticPlot: true,
     displayModeBar: false,
